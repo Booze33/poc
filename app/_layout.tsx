67131f5d7ea7846fa8ab97234283a5bfd28974b9
Booze33 <tebetisloh@gmail.com>
2025-01-05 09:36:44 +0100

    app setup

---
+++ app/_layout.tsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import { View, Text } from 'react-native'
+import { ThemeProvider } from '@/context/theme.context'
+import { Stack } from 'expo-router'
+
+export default function RootLayout() {
+
+  return (
+    <ThemeProvider>
+      <Stack screenOptions={{headerShown: false}}>
+        <Stack.Screen name="index" />
+        <Stack.Screen name="(routes)/onboarding/index" />
+        
+      </Stack>
+    </ThemeProvider>
+  );
+}
