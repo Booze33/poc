0a3d310f4a01aae299a82431f1a0ae89d8ab2f62
Booze33 <tebetisloh@gmail.com>
2025-01-10 18:30:03 +0100

    app auth

--- app/index.tsx
+++ app/index.tsx
@@ -1,30 +1,27 @@
-import React, { useState, useEffect } from 'react'
-import { View, Text } from 'react-native'
-import * as SecureStore from 'expo-secure-store'
+import React, { useEffect, useState } from "react";
+import * as SecureStore from "expo-secure-store";
 import { Redirect } from "expo-router";
 
 export default function index() {
-  const [loggedInUser, setLoggedInUser] = useState(false);
-  const [loading, setLoading] = useState(false);
+  const [loggedInUser, setloggedInUser] = useState(false);
+  const [loading, setLoading] = useState(true);
 
   useEffect(() => {
     const subscription = async () => {
-      const token = await SecureStore.getItem('accessToken');
-      setLoggedInUser(token ? true : false);
+      const token = SecureStore.getItem("accessToken");
+      setloggedInUser(token ? true : false);
       setLoading(false);
     };
     subscription();
-  }, [])
+  }, []);
 
   return (
     <>
-      {
-        loading ? 
+      {loading ? (
         <></>
-        : (
-          <Redirect href={!loggedInUser ? '/(routes)/onboarding' : '/(tabs)'} />
-        )
-      }
+      ) : (
+        <Redirect href={!loggedInUser ? "/(routes)/onboarding" : "/(tabs)"} />
+      )}
     </>
   );
 }
