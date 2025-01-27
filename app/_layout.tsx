717ae4e4cbe4f722e5ae2b78e2886baec5ea6159
Booze33 <tebetisloh@gmail.com>
2025-01-27 11:03:10 +0100

    app notification

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -29,7 +29,7 @@ export default function RootLayout() {
       <Stack screenOptions={{headerShown: false}}>
         <Stack.Screen name="index" />
         <Stack.Screen name="(routes)/onboarding/index" />
-        
+        <Stack.Screen name="(routes)/notification/index" />
       </Stack>
     </ThemeProvider>
   );
